ea8e0baa6bcecbcd472e8e6fd6475a4868817754
Wei Yang <weix0015@stud.kea.dk>
2025-02-18 15:23:00 +0100

    tilføjet employee og views

--- index.php
+++ index.php
@@ -1,8 +1,51 @@
 <?php
 
-require_once 'src/database.php';
+require_once 'src/employee.php';
 
-echo '<pre>';
+$searchText = trim($_GET['search'] ?? '');
 
 $pdo = connect();
-print_r(getAllEmployees($pdo));
+if ($searchText === '') {
+    $employees = getAllEmployees($pdo);
+} else {
+    $employees = searchEmployees($pdo, $searchText);
+}
+if (!$employees) {
+    $errorMessage = 'There was an error while retrieving the list of employees.';
+}
+
+include_once 'views/header.php';
+?>
+    <nav>
+        <ul>
+            <li><a href="new.php" title="Create new employee">Add employee</a></li>
+        </ul>
+    </nav>
+    <main>
+        <?php if (isset($errorMessage)): ?>
+            <section>
+                <p class="error"><?=$errorMessage ?></p>
+            </section>
+        <?php else: ?>
+            <form action="index.php" method="GET">
+                <div>
+                    <label for="txtSearch">Search</label>
+                    <input type="search" id="txtSearch" name="search">
+                </div>
+                <div>
+                    <button type="submit">Search</button>
+                </div>
+            </form>
+            <section>
+                <?php foreach ($employees as $employee): ?>
+                    <article>
+                        <p><strong>First name: </strong><?=$employee['cFirstName'] ?></p>
+                        <p><strong>Last name: </strong><?=$employee['cLastName'] ?></p>
+                        <p><strong>Birth date: </strong><?=$employee['dBirth'] ?></p>
+                        <p><a href="view.php?id=<?=$employee['nEmployeeID'] ?>">View details</a></p>
+                    </article>
+                <?php endforeach; ?>
+            </section>
+        <?php endif; ?>
+    </main>
+<?php include_once 'views/footer.php'; ?>
